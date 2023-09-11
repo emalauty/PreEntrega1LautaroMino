b787f8b9ec2f62000fc1c1bb03594df4ee45156b
emalauty <lautaroaburrido1214@gmail.com>
2023-09-11 14:08:13 -0300

    incorporacion de los arrays a mi proyecto

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //function alumnos(){
-//     for (let alumnos = 1; alumnos < 11; alumnos++){
-//          estudiantes = prompt("Buenas maestro, ingrese el nombre del alumno.");
-//          nota = parseInt(prompt("Ahora ingrese las notas de cada alumno para ver si aprobo o desaprobo."));
+ //    for (let alumnos = 1; alumnos < 11; alumnos++){
+ //         estudiantes = prompt("Buenas maestro, ingrese el nombre del alumno.");
+ //         nota = parseInt(prompt("Ahora ingrese las notas de cada alumno para ver si aprobo o desaprobo."));
 //
 //          console.log(estudiantes+" se saco un "+nota);
 //          if (nota >= 7 && nota <= 10){
@@ -16,19 +16,41 @@
 //     }
 //}
 //alumnos()
+// CALCULADORA
 
-const suma = (a, b) => a + b;
 
-const resta = (a, b) => a - b;
+//const resta = (a, b) => a - b;
 
-const divison = (a, b) => a / b;
+//const divison = (a, b) => a / b;
 
-const multiplicacion = (a, b) => a * b;
+//const multiplicacion = (a, b) => a * b;
 
-const iva = (a) => a * 0.21;
+//const iva = (a) => a * 0.21;
 
-console.log(suma(18, 9));
-console.log(resta(5, 2));
-console.log(divison(9, 3));
-console.log(multiplicacion(2, 3));
-console.log(iva(500));
+//console.log(resta(5, 2));
+
+
+let materias = ["lengua", "matematicas", "ingles", "quimica", "informatica"]
+materias.push('gimnasia')
+
+
+console.log(("estas son las matertias " + materias.join(" - ")))
+function alumnos(){
+     for (let alumnos = 1; alumnos < 4; alumnos++){
+          estudiantes = prompt("Buenas maestro, ingrese el nombre del alumno.");
+          materias = prompt("Ahora ingrese la materia")
+          nota = parseInt(prompt("Ahora ingrese las notas de cada alumno para ver si aprobo o desaprobo."));
+
+          console.log(estudiantes+" se saco un "+nota+" en "+ materias);
+          if (nota >= 7 && nota <= 10){
+               console.log("Este estudiante ha aprobado "+ materias);
+          }
+         else if (nota < 7){
+               console.log("Este estudiante ha desaprobado "+ materias);
+          }
+          else if (nota > 10){
+               console.log("la nota maximo de nota es un "+ 10);
+         }
+     }
+}
+alumnos()
